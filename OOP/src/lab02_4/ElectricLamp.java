aceb5bc9b980fe036eb33a1c109df2f45c1056a6
soul0silver <122255949+soul0silver@users.noreply.github.com>
2023-09-05 16:56:30 +0700

    ds

---
+++ OOP/src/lab02_4/ElectricLamp.java
@@ -0,0 +1,21 @@
+package lab02_4;
+
+public class ElectricLamp {
+    private boolean status;
+
+    public ElectricLamp() {
+        status=false;
+    }
+    public void turnOn(){
+        if (status==false) {
+            status=true;
+            System.out.println("Lamp is lighting");}
+        else System.out.println("Lamp is already be turn on");
+    }
+    public void turnOff(){
+        if (status==true) {
+            status = false;
+            System.out.println("Lamp is off");
+        } else System.out.println("Lamp is already off");
+    }
+}
